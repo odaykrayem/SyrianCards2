7f5423b8227c16350b4ffc8a23b54b9c78607072
odaykrayem <0day.krayem0@gmail.com>
2022-02-08 21:45:48 +0200

    first

--- app/src/main/java/com/mrmindteam/syriancards/Activities/HomeActivity.java
+++ app/src/main/java/com/mrmindteam/syriancards/Activities/HomeActivity.java
@@ -1,16 +1,29 @@
 package com.mrmindteam.syriancards.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.mrmindteam.syriancards.R;
 
+import jp.wasabeef.blurry.Blurry;
+
 public class HomeActivity extends AppCompatActivity {
 
+    CardView card1;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        frameLayout = findViewById(R.id.frame);
+        Blurry.with(this)
+                .sampling(8)
+                .color(Color.argb(66, 255, 255, 0))
+                .async()
+                .onto(frameLayout);
     }
 }
