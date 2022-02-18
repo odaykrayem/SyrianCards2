01c67de1ad2f74ebbc381526f5827b189c269455
odaykrayem <0day.krayem0@gmail.com>
2022-02-18 16:43:44 +0200

    fifth

--- app/src/main/java/com/mrmindteam/syriancards/activities/BalanceRequestsActivity.java
+++ app/src/main/java/com/mrmindteam/syriancards/activities/BalanceRequestsActivity.java
@@ -1,17 +1,76 @@
 package com.mrmindteam.syriancards.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 
+import com.mrmindteam.syriancards.Constants;
 import com.mrmindteam.syriancards.R;
+import com.mrmindteam.syriancards.adapters.BalanceRequestsAdapter;
+import com.mrmindteam.syriancards.models.Request;
 
-public class BalanceRequestsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class BalanceRequestsActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+
+
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView rv;
+    BalanceRequestsAdapter adapter;
+    ArrayList<Request> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balance_requests);
+
+        rv = findViewById(R.id.rv_requests);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        list = new ArrayList<Request>(){{
+            new Request(1, "mesage", 3400, Constants.BALANCE_REQUEST_STATUS_DONE, 1,"12-2-2022");
+            new Request(1, "mesage", 3400, Constants.BALANCE_REQUEST_STATUS_DONE, 1,"12-2-2022");
+            new Request(1, "mesage", 3400, Constants.BALANCE_REQUEST_STATUS_DONE, 1,"12-2-2022");
+            new Request(1, "mesage", 3400, Constants.BALANCE_REQUEST_STATUS_DONE, 1,"12-2-2022");
+            new Request(1, "mesage", 3400, Constants.BALANCE_REQUEST_STATUS_DONE, 1,"12-2-2022");
+
+
+        }};
+        adapter = new BalanceRequestsAdapter(getApplicationContext(),list);
+        rv.setAdapter(adapter);
+//        getRequests();
+
+//        mSwipeRefreshLayout.setOnRefreshListener(this);
+//        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
+//                android.R.color.holo_green_dark,
+//                android.R.color.holo_orange_dark,
+//                android.R.color.holo_blue_dark);
+//
+//
+//        mSwipeRefreshLayout.post(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//
+//                mSwipeRefreshLayout.setRefreshing(true);
+//                getRequests();
+//
+//            }
+//        });
+
+
+
+    }
+
+    private void getRequests() {
+
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onRefresh() {
+       getRequests();
 
 
     }
--- app/src/main/java/com/mrmindteam/syriancards/activities/MyOrdersActivity.java
+++ app/src/main/java/com/mrmindteam/syriancards/activities/MyOrdersActivity.java
@@ -1,16 +1,75 @@
 package com.mrmindteam.syriancards.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 
+import com.mrmindteam.syriancards.Constants;
 import com.mrmindteam.syriancards.R;
+import com.mrmindteam.syriancards.adapters.BalanceRequestsAdapter;
+import com.mrmindteam.syriancards.adapters.MyOrdersAdapter;
+import com.mrmindteam.syriancards.models.Order;
+import com.mrmindteam.syriancards.models.Request;
+
+import java.util.ArrayList;
 
 public class MyOrdersActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView rv;
+    MyOrdersAdapter adapter;
+    ArrayList<Order> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_orders);
+
+        rv = findViewById(R.id.rv_orders);
+        list = new ArrayList<Order>(){{
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+        }};
+        adapter = new MyOrdersAdapter(getApplicationContext(),list);
+        rv.setAdapter(adapter);
+//        mSwipeRefreshLayout.setOnRefreshListener(this);
+//        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
+//                android.R.color.holo_green_dark,
+//                android.R.color.holo_orange_dark,
+//                android.R.color.holo_blue_dark);
+//
+//
+//        mSwipeRefreshLayout.post(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//
+//                mSwipeRefreshLayout.setRefreshing(true);
+//                 getOrders();
+//            }
+//        });
+
+
+
+
     }
+
+    private void getOrders() {
+        list = new ArrayList<Order>(){{
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+            new Order (1,"google", "25", 2, "12-2-2022", Constants.CARD_STATUS_ACCEPTED);
+        }};
+        adapter = new MyOrdersAdapter(getApplicationContext(),list);
+        rv.setAdapter(adapter);
+        mSwipeRefreshLayout.setRefreshing(false);
+
+    }
+
 }
